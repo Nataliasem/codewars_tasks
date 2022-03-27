7450a7c9aa4f647d890aae501d5315410409214f
natalia <seraya.93@inbox.ru>
2022-03-27 20:52:16 +0300

    files renamed

--- tasks/sum-odd-numbers.js
+++ tasks/sum-odd-numbers.js
@@ -1,4 +1,4 @@
-function sum_odd_numbers(rawNumbers) {
+function sumOddNumbers(rawNumbers) {
     const odd = rawNumbers.filter(item => item % 2 !== 0)
 
     return odd.reduce((previous, current) => {
